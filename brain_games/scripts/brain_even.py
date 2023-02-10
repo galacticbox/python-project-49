508420e2eee82fc43c9461ed13e0450df2a68679
Gala K <step.by.step.jb@gmail.com>
2023-02-10 14:17:35 +0300

    even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic_even import welcome_user, game_even
+
+def main():
+    welcome_user()
+    game_even()
+    pass
+
+
+if __name__ == '__main__':
+    main()
