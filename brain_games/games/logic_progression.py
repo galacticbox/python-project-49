835328aa4a2c0b4df20564d38f448f9f832f591f
Gala K <step.by.step.jb@gmail.com>
2023-02-12 01:20:21 +0300

    2

--- brain_games/games/logic_progression.py
+++ brain_games/games/logic_progression.py
@@ -8,7 +8,7 @@ def game_prog():
         start = randint(1, 10)
         stop = randint(40, 70)
         lis = [i for i in range(start, stop, step)]
-        missing_num_index = randint(1, len(lis))
+        missing_num_index = randint(1, len(lis) - 1)
         answer_num = lis.pop(missing_num_index)
         lis.insert(missing_num_index, '..')
         print('Question:', *lis, sep=' ')
